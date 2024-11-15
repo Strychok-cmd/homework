f9f50ca47e4f950b7bca8c80fdf31f6668d352ef
Strychok-cmd <vladislavsemikov16@gmail.com>
2024-11-15 08:02:18 +0300

    Add files via upload

--- task3.py
+++ task3.py
@@ -1,2 +1,13 @@
-#3rd program
-print(2 * 2 +2 == 2 * (2+2))
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+First_ = 0
+while First_ < len(my_list) :
+    number = my_list[First_]
+    First_ = First_ + 1
+    if number == 0 :
+        continue
+    elif number < 0 :
+        break
+    elif First_ == len(my_list) :
+        print(number)
+    else:
+        print(number)
